8d1608a924ee15ab9bd9cab1077f10ea0347e020
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2024-05-30 06:41:41 +0900

    認証情報の読み込み追加

--- html/js/index.js
+++ html/js/index.js
@@ -1,11 +1,11 @@
 "use strict";
 
-function init()
+function main()
 {
-  // ペースト
+  // ペーストボタン
   var btn_code_paste = document.getElementById("btn_code_paste");
   var txt_auth_code = document.getElementById("txt_auth_code");
-  btn_code_paste.addEventListener("click", function(){
+	btn_code_paste.addEventListener("click", function(){
     navigator.clipboard.readText()
       .then(text => {
         txt_auth_code.value = text;
@@ -14,10 +14,31 @@ function init()
         txt_auth_code.value = "error";
       });
   });
-}
-function main()
-{
-  init();
+  
+	// 認証情報読み込み
+	// ⚠️認証情報を公開するのは本来はNG。ローカルテスト用の実装。
+	// js を読み込んだhymlから見た JSONファイルのURL
+var url = 'secret/credential.json';
+
+// fetchを使用してJSONファイルを読み込む
+fetch(url)
+  .then(response => {
+    // レスポンスが正常かチェック
+    if (!response.ok) {
+      throw new Error('ネットワークレスポンスが正常ではありません');
+    }
+    // レスポンスをJSONとして解析
+    return response.json();
+  })
+  .then(data => {
+    // 解析されたJSONデータを使用
+    console.log(data);
+  })
+  .catch(error => {
+    // エラー処理
+		console.error('読み込み中にエラーが発生しました:', error);
+  });
+
 }
 
 
