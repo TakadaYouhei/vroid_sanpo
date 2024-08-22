244e03ecb53e40389922baef857cb822f56bdbdd
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2024-08-22 13:04:00 +0900

    sensor control 作成中

---
+++ html/js/SensorControls.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/*
+ * ジャイロと加速度センサーで object の向きと位置をコントロールする
+ */
+class SensorControls {
+  constructor( object, domElemnt ) {
+    this.object = object;
+    this.domElement = domElement;
+    
+    this.dispose = function() {
+      window.removeEventListener("devicemotion", onDeviceMotion);
+      window.removeEventListener("deviceorientation", onDeviceOrientation);
+    }
+    
+    const scope = this;
+    
+    function onDeviceMotion(event) {
+      const ax = event.accelerationIncludingGravity.x;
+      const ay = event.accelerationIncludingGravity.y;
+      const az = event.accelerationIncludingGravity.z;
+      
+      scope.object.position.x = ax;
+      scope.object.position.y = ay + 9.8;
+      scope.object.position.z = az;
+      scope.object.updateMatixWorld();
+    }
+    
+    function onDeviceOrientation(event) {
+      
+    }
+    
+    window.addEventListener("devicemotion", onDeviceMotion);
+    window.addEventListener("deviceorientation", onDeviceOrientation);
+  } // end of constructor
+}
+
+export { SensorControls };
